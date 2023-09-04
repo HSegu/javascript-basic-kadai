c802f7b2ee700b640ab02368d47f6bfaeebcadbb
HSegu <h.segchi@gmail.com>
2023-09-05 01:03:20 +0900

    課題13 提出

---
+++ kadai_013/square.js
@@ -0,0 +1,18 @@
+const val1 = 3;
+const val2 = 5;
+
+
+
+const squareDisp1 = (val) => {
+   return val ** 2;
+}
+
+console.log(squareDisp1(val1));
+
+
+
+function squareDisp2(val){
+    return val ** 2;
+}
+
+console.log(squareDisp2(val2));
